21e8b9b88304f7b0121df0e1a530711b7342a844
Senczuk <williamjsr97@gmail.com>
2026-02-27 13:50:04 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  console.log("Service Worker ativo");
+});
+
+self.addEventListener("fetch", event => {});
